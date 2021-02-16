69878d5d72421e701f03b193d0bd622307625990
Itzbenz <gamerrindoubuntu@gmail.com>
2021-02-16 11:32:16 +0100

    lower score = better ?

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 a neural network that imitate a computational thing\
-![](https://cdn.discordapp.com/attachments/713346278003572777/811094140997206026/unknown.png)
+score: lower better accuracy: higher better
+![](https://cdn.discordapp.com/attachments/809580979777568828/811182724114284594/unknown.png)
 ![](https://cdn.discordapp.com/attachments/713346278003572777/811093717008121926/unknown.png)
 ![](https://cdn.discordapp.com/attachments/713346278003572777/811093350618890320/unknown.png)
+
+```mermaid
+>eval
+
+
+========================Evaluation Metrics========================
+ # of classes:    2
+ Accuracy:        0.7400
+ Precision:       0.0000
+ Recall:          0.0000
+ F1 Score:        0.0000
+Precision, recall & F1: reported for positive class (class 1 - "1") only
+
+Warning: 1 class was never predicted by the model and was excluded from average precision
+Classes excluded from average precision: [1]
+
+=========================Confusion Matrix=========================
+  0  1
+-------
+ 37  0 | 0 = 0
+ 13  0 | 1 = 1
+
+Confusion matrix format: Actual (rowClass) predicted as (columnClass) N times
+==================================================================
+>test
+Temperature, Humidity, Prefer to use jacket (float)
+[[            -38,             16,    0.710907042]]
+Output:
+[[      0.6892066,     0.31079337]]
+Desired Output:
+[[              1,              0]]
+Should Use jacket: Yes
+NeuralNet Answer: Yes
+Score: 0.3722141981124878
+>score
+0.6492486409842968
+>min
+0.5786642402410507
+>train
+Accuracy Before: 0.66
+Accuracy After: 0.66
+>score
+0.6476687848567962
+>min
+0.5786642402410507
+>
+```
--- src/main/java/SDL1/Main.java
+++ src/main/java/SDL1/Main.java
@@ -4,6 +4,7 @@ import Atom.Utility.Utility;
 import SimpleLM1.SimpleLearning;
 import org.deeplearning4j.core.storage.StatsStorage;
 import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -12,7 +13,7 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.ui.api.UIServer;
 import org.deeplearning4j.ui.model.stats.StatsListener;
 import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
-import org.nd4j.linalg.activations.Activation;
+import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.cpu.nativecpu.NDArray;
 import org.nd4j.linalg.dataset.DataSet;
@@ -21,14 +22,15 @@ import org.nd4j.linalg.learning.config.Adam;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class Main {
 	public static File model = new File("SDL1.zip");
-	public static double minScore = 0.5, learningRate = 0.001;
+	public static double minScore = 1, learningRate = 0.001;
 	
 	public static void main(String[] args) throws IOException {
 		org.apache.log4j.BasicConfigurator.configure();
-		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder().updater(new Adam(learningRate)).list().layer(new DenseLayer.Builder().nIn(3).nOut(2).build()).layer(new DenseLayer.Builder().nIn(2).nOut(2).activation(Activation.RELU).build()).layer(new OutputLayer.Builder().nIn(2).nOut(2).build()).build();
+		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder().updater(new Adam(learningRate)).optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).list().layer(new DenseLayer.Builder().nIn(3).nOut(2).build()).layer(new DenseLayer.Builder().nIn(2).nOut(2).build()).layer(new OutputLayer.Builder().nIn(2).nOut(2).build()).build();
 		
 		MultiLayerNetwork nets;
 		try {
@@ -58,21 +60,23 @@ public class Main {
 					DataSet ds = getDataSet();
 					
 					
-					INDArray d = ds.getFeatures();
+					INDArray input = ds.getFeatures();
+					INDArray dOutput = ds.getLabels();
 					System.out.println("Temperature, Humidity, Prefer to use jacket (float)");
-					System.out.println(d.toStringFull());
-					INDArray output = net.activate(d, Layer.TrainingMode.TEST);
+					System.out.println(input.toStringFull());
+					INDArray output = net.output(input, Layer.TrainingMode.TEST);
 					System.out.println("Output:");
 					System.out.println(output.toStringFull());
 					System.out.println("Desired Output:");
-					System.out.println(ds.getLabels().toStringFull());
-					System.out.println("Should Use jacket: " + (shouldUse(d) ? "Yes" : "No"));
+					System.out.println(dOutput.toStringFull());
+					System.out.println("Should Use jacket: " + (shouldUse(input) ? "Yes" : "No"));
 					System.out.println("NeuralNet Answer: " + (use(output) ? "Yes" : "No"));
 					System.out.println("Score: " + net.score(ds));
 				}catch (Throwable t) {
 					t.printStackTrace();
 				}
 			}
+			if (s.equalsIgnoreCase("eval")) System.out.println(evaluation(Main::getDataSet, net).stats());
 			if (s.equalsIgnoreCase("min")) System.out.println(minScore);
 			if (s.equalsIgnoreCase("score")) System.out.println(getAverageScore(net));
 			if (s.equalsIgnoreCase("train")) {
@@ -88,7 +92,7 @@ public class Main {
 	
 	public static double getAverageScore(MultiLayerNetwork nets) {
 		ArrayList<Double> floats = new ArrayList<>();
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 40; i++) {
 			floats.add(nets.score(getDataSet()));
 		}
 		return floats.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
@@ -116,15 +120,34 @@ public class Main {
 		return SimpleLearning.shouldUseJacket(nd.toFloatVector());
 	}
 	
+	public static Evaluation evaluation(Function<Void, DataSet> d, MultiLayerNetwork net) {
+		Evaluation evaluation = new Evaluation(2);
+		
+		for (int i = 0; i < 50; i++) {
+			DataSet set = d.apply(null);
+			INDArray input = set.getFeatures(), dOutput = set.getLabels();
+			evaluation.eval(dOutput, net.output(input));
+		}
+		
+		return evaluation;
+	}
+	
 	public static void train(MultiLayerNetwork net) throws IOException {
+		
+		System.out.println("Accuracy Before: " + evaluation(Main::getDataSet, net).accuracy());
 		for (int i = 0; i < 5; i++) {
-			while (net.score() < minScore - learningRate) {
+			double fit = getAverageScore(net);
+			while (fit > minScore - learningRate) {
 				DataSet d = getDataSet();
 				net.fit(d);
+				
+				
+				fit = getAverageScore(net);
 			}
-			minScore = Math.min(minScore, net.score());
+			minScore = Math.min(minScore, getAverageScore(net));
 		}
 		net.save(model, true);
+		System.out.println("Accuracy After: " + evaluation(Main::getDataSet, net).accuracy());
 	}
 	
 	public static NDArray randomData() {
@@ -133,6 +156,10 @@ public class Main {
 	
 	public static NDArray randomData(float[] in) {
 		return new NDArray(in);
+	}
+	
+	public static DataSet getDataSet(Void v) {
+		return getDataSet();
 	}
 	
 	public static DataSet getDataSet() {
